6846352dba2829611428e77a01f3f29389075e6f
geonjunk <koeunjun7@gmail.com>
2025-02-20 00:14:28 +0900

    feat : questionId 자동 증감 추가

--- src/main/java/com/dnd/demo/domain/feedback/entity/FeedbackQuestion.java
+++ src/main/java/com/dnd/demo/domain/feedback/entity/FeedbackQuestion.java
@@ -1,6 +1,7 @@
 package com.dnd.demo.domain.feedback.entity;
 
 import com.dnd.demo.domain.project.enums.QuestionType;
+import jakarta.persistence.Id;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class FeedbackQuestion {
 
+    @Id
     private String questionId;
     private String question;
     private QuestionType type;

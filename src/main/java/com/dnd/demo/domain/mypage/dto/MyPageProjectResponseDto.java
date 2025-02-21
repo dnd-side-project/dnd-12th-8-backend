1da8f1736d2dd604fbee9215a69ed5ce526cd75d
이건희 <dnzp74@g.eulji.ac.kr>
2025-02-21 13:25:19 +0900

    refactor: myPage에 카테고리 정보도 추가

--- src/main/java/com/dnd/demo/domain/mypage/dto/MyPageProjectResponseDto.java
+++ src/main/java/com/dnd/demo/domain/mypage/dto/MyPageProjectResponseDto.java
@@ -1,6 +1,13 @@
 package com.dnd.demo.domain.mypage.dto;
 
+import java.util.List;
+
+import com.dnd.demo.domain.project.dto.response.PlatformCategoryResponse;
 import com.dnd.demo.domain.project.entity.Project;
+import com.dnd.demo.domain.project.entity.ProjectCategory;
+import com.dnd.demo.domain.project.enums.Job;
+import com.dnd.demo.domain.project.enums.PlatformType;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,14 +22,23 @@ public class MyPageProjectResponseDto {
     private String logoImgUrl;
     private String thumbnailImgUrl;
     private String description;
+    private String dueDate;
+    private Job job;
+    private PlatformCategoryResponse platformCategoryResponse;
 
-    public static MyPageProjectResponseDto fromProject(Project project) {
+    public static MyPageProjectResponseDto fromProject(Project project, PlatformCategoryResponse platformCategoryResponse) {
         return MyPageProjectResponseDto.builder()
-          .projectId(project.getProjectId())
-          .title(project.getTitle())
-          .logoImgUrl(project.getLogoImgUrl())
-          .thumbnailImgUrl(project.getThumbnailImgUrl())
-          .description(project.getDescription())
-          .build();
+            .projectId(project.getProjectId())
+            .title(project.getTitle())
+            .logoImgUrl(project.getLogoImgUrl())
+            .thumbnailImgUrl(project.getThumbnailImgUrl())
+            .description(project.getDescription())
+            .dueDate(project.getDueDate())
+            .job(project.getTargetJob())
+            .platformCategoryResponse(platformCategoryResponse)
+            .build();
     }
 }
+
+
+
--- src/main/java/com/dnd/demo/domain/project/repository/ProjectCategoryRepository.java
+++ src/main/java/com/dnd/demo/domain/project/repository/ProjectCategoryRepository.java
@@ -3,6 +3,8 @@ package com.dnd.demo.domain.project.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dnd.demo.domain.project.entity.ProjectCategory;
@@ -12,4 +14,6 @@ public interface ProjectCategoryRepository extends JpaRepository<ProjectCategory
 	List<ProjectCategory> findByProjectId(Long id);
 
 	void deleteByProjectId(Long id);
-}
+
+	@Query("SELECT pc.categoryId FROM ProjectCategory pc WHERE pc.projectId = :projectId")
+	List<Long> findCategoryIdsByProjectId(@Param("projectId") Long projectId);}
